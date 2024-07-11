abd379eb1a6bf58d8ff24d1b10db2e1271a30479
Lucas C Lyra Pereira <148347259+L-C-Lyra@users.noreply.github.com>
2024-07-11 10:36:46 -0300

    Added main game menu, game screen.

---
+++ game_menu.py
@@ -0,0 +1,8 @@
+from PPlay.window import *
+from PPlay.gameimage import *
+from PPlay.sprite import *
+from PPlay.keyboard import *
+from PPlay.mouse import *
+import start_menu
+import main_menu
+import play_menu
